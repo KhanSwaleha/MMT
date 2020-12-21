6ed0251cc09b421f04b0e5062d5cbda0fad508af
Swaleha Khan <swalu41@gmail.com>
2020-12-21 16:49:53 +0530

    'initial'

---
+++ src/test/java/com/mmt/utils/PropertyReader.java
@@ -0,0 +1,25 @@
+package com.mmt.utils;
+
+import java.io.FileInputStream;
+
+import java.util.Properties;
+
+public class PropertyReader {
+	
+	private static Properties propInstance;
+	
+	public static Properties getInstance() {
+		
+		if (propInstance == null) {
+			propInstance = new Properties();
+			try {
+				propInstance.load(new FileInputStream(".\\src\\test\\resources\\setting.property"));
+			} catch (Exception e) {
+				System.out.println("Error with reading property File " + e.getMessage());
+			}
+			return propInstance;
+		} else {
+			return propInstance;
+		}
+	}
+}
